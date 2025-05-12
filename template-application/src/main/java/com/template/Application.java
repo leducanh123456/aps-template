b8c78afb603f9a9740532bc2b692ef2d6952b434
leducanh.anhld <leducanh.anhld@gmail.com>
2025-05-12 09:50:36 +0700

    first commit

---
+++ template-application/src/main/java/com/template/Application.java
@@ -0,0 +1,8 @@
+package com.template;
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
